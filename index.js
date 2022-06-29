4d081420ab3a969fdf6a17d23a1fcbdc5de26b18
Erykzon <69286881+erykzon@users.noreply.github.com>
2022-06-29 19:07:15 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log("Hello World!")
+console.log(1150)
+
+const user = {
+  name: "eri",
+  lastnanme: "key",
+  showfullname:
+    function showfullname() {
+      return "Erick G"
+    },
+  age: 30,
+  hobbie: ["leer", "progrmar", "runb"],
+  address: {
+    street: "some",
+    city: "somewhere",
+
+  }
+}
+
+console.log(user.showfullname())
