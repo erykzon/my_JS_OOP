838b6e7b3ee0d59322f63b8bcfd8ea18fdd82b35
Erykzon <69286881+erykzon@users.noreply.github.com>
2022-07-04 18:04:57 +0000

    Creando constructor

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-console.log("Hello World!")
-console.log(1150)
-
 const user = {
   name: "eri",
   lastnanme: "key",
-  showfullname:
-    function showfullname() {
-      return "Erick G"
+  showfullname() {
+      return `${this.name} ${this.lastnanme}`
     },
-  age: 30,
-  hobbie: ["leer", "progrmar", "runb"],
+  age: 30
+/*   hobbie: ["leer", "progrmar", "runb"],
   address: {
     street: "some",
     city: "somewhere",
-
-  }
+  } */
 }
-
 console.log(user.showfullname())
+
+function Person (){
+	this.name = ""
+	this.lastname = ""
+	this.age = ""
+	this.showfullname = function (){
+		return `${this.name} ${this.lastname}`
+	}
+}
