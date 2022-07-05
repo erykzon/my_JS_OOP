6c07ca44b210643f73423cc58bafe2e10209e5e7
Erykzon <69286881+erykzon@users.noreply.github.com>
2022-07-05 16:54:26 +0000

    Modificando json

--- cons_object.js
+++ cons_object.js
@@ -1,3 +1,44 @@
 const number = new Object (12)
 console.log(number)
 
+const user = {
+  name: "eri",
+  lastnanme: "key",
+  showfullname() {
+      return `${this.name} ${this.lastnanme}`
+    },
+  age: 30,
+  hobbie: ["leer", "progrmar", "runb"],
+  address: {
+    street: "some",
+    city: "somewhere",
+  }
+}
+console.log(user.showfullname())
+console.log(Object.keys(user))
+console.log(Object.values(user))
+
+function Person (){
+	this.name = ""
+	this.lastname = ""
+	this.age = ""
+	this.showfullname = function (){
+		return `${this.name} ${this.lastname}`
+	}
+}
+
+const user2 = new Person()
+user2.name = "Pato"
+user2.lastname = "Loco"
+user2.age = 25
+console.log(user2.showfullname())
+
+const user3 = new Person()
+user3.name = "Chganchitoi"
+user3.lastname = "Feliz"
+user3.age = 45
+console.log(user3.showfullname())
+
+
+const number = new Object (12)
+console.log(number)
